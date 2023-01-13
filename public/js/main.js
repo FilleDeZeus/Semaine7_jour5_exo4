032cb6e30b055550061fca6ad17619c7b859d5c4
FilleDeZeus <enide26@hotmail.fr>
2023-01-13 12:13:06 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+//----EXERCICE 1
+
+let red = document.getElementsByClassName("redPurple");
+console.log(red);
+
+//----EXERCICE 2
+let red1 = document.querySelectorAll(".redPurple");
+console.log(red1);
+
+//----EXERCICE 3
+let recup = document.querySelectorAll("h1.redPurple");
+console.log(recup);
+
+//----EXERCICE 4
+let ttrecup = document.querySelectorAll("li,p,span");
+console.log(ttrecup);
+
+//----EXERCICE 5
+let element = document.querySelectorAll("li.important, p");
+console.log(element);
+
+//----EXERCICE 6
+let h1 = document.querySelectorAll("h1.redPurple, span.redPurple");
+console.log(h1);
